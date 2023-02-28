86b8795534970008dc5699f3140d4f4c7799f7b0
Adarsh Gupta <adarsh.gupta@aot.edu.in>
2023-02-28 19:58:26 +0530

    Password Encryption

---
+++ backend/models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose")
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true,"Please add a Name"]
+    },
+    email:{
+        type: String,
+        required: [true,"Please add an Email"],
+        unique: true,
+        trim: true,
+        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,"Please enter a vaild email"]
+    },
+    password:{
+        type: String,
+        required: [true,"Please add a Password"],
+        minLength:[6,"Password Must be upto 6 characters"],  
+    },
+    photo:{
+        type:String,
+        required: [true,"please upload a photo"],
+        default:"https://cdn-icons-png.flaticon.com/512/20/20079.png"
+    },
+    phone:{
+        type: String,
+        default: "+91"
+    },
+    bio:{
+        type: String,
+        maxLength:[250,"Bio must not be more than 250 character"],
+        default:"bio",
+    },
+}, {
+    timestamps: true
+})
+
+const User = mongoose.model("User",userSchema)
+module.exports = User
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const userRoute = require("./routes/userRoute")
+const errorHandler = require("./middleWare/errorMiddleWare")
 const app = express();
+
+//MiddleWares
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.json())
+  
+    // Routes Middleware
+app.use("/api/users",userRoute)
+//Routes
+app.get("/",(req,res)=>{
+    res.send("Home page");
+});
 
 const PORT = process.env.PORT || 5000;
 mongoose.set('strictQuery', false);
+// Error MiddleWare
+app.use(errorHandler);
 // Connect to DB and start server
 mongoose
   .connect(process.env.MONGO_URI)
