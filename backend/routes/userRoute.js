3168850e614a215992ef2f7c734d4acc1cfaff3d
Adarsh Gupta <adarsh.gupta@aot.edu.in>
2023-03-01 16:29:47 +0530

    Change Password

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { registerUser, loginUser, logout,getUser, loginStatus, updateUser } = require("../controllers/userController");// 
+const { registerUser, loginUser, logout,getUser, loginStatus, updateUser, changePassword } = require("../controllers/userController");// 
 const protect = require("../middleWare/authMiddleware");
 const router = express.Router();
 
@@ -9,4 +9,5 @@ router.get("/logout",logout);
 router.get("/getuser", protect, getUser);
 router.get("/loggedin",loginStatus);
 router.patch("/updateuser",protect,updateUser);
+router.patch("/changepassword",protect,changePassword);
 module.exports = router;
