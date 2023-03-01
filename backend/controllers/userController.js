1cc1c938f8d8e6b36fbfc013ff4824bf4137460d
Adarsh Gupta <adarsh.gupta@aot.edu.in>
2023-03-01 16:09:11 +0530

    Update User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -121,13 +121,67 @@ const logout = asyncHandler( async(req,res)=>{
   return res.status(200).json({ message: "Successfully Logged Out" });
 })
 //Get user
-const getuser = asyncHandler( async(req,res)=>{
-  res.send("Userdata");
-})
+const getUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
 
+  if (user) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+    });
+  } else {
+    res.status(400);
+    throw new Error("User Not Found");
+  }
+});
+// Login Status
+  const loginStatus = asyncHandler(async(req,res) =>{
+    const token = req.cookies.token;
+    if (!token) {
+      return res.json(false);
+    }
+    // Verify Token
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if (verified) {
+      return res.json(true);
+    }
+    return res.json(false);
+  })
+  const updateUser = asyncHandler( async(req,res)=>{
+    const user = await User.findById(req.user._id);
+
+  if (user) {
+    const { name, email, photo, phone, bio } = user;
+    user.email = email;
+    user.name = req.body.name || name;
+    user.phone = req.body.phone || phone;
+    user.bio = req.body.bio || bio;
+    user.photo = req.body.photo || photo;
+
+    const updatedUser = await user.save();
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      photo: updatedUser.photo,
+      phone: updatedUser.phone,
+      bio: updatedUser.bio,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+  })
 module.exports = {
   registerUser,
   loginUser,
   logout,
-  getuser,
+  getUser,
+  loginStatus,
+  updateUser,
 };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { registerUser, loginUser, logout, getuser } = require("../controllers/userController");
+const { registerUser, loginUser, logout,getUser, loginStatus, updateUser } = require("../controllers/userController");// 
+const protect = require("../middleWare/authMiddleware");
 const router = express.Router();
 
 router.post("/register",registerUser)
 router.post("/login",loginUser);
 router.get("/logout",logout);
-router.get("/getuser",getuser);
+router.get("/getuser", protect, getUser);
+router.get("/loggedin",loginStatus);
+router.patch("/updateuser",protect,updateUser);
 module.exports = router;
