eeace1325f9c3884574efb42a56bee5e914089d2
Adarsh Gupta <adarsh.gupta@aot.edu.in>
2023-03-06 16:23:00 +0530

    frontend

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../redux/auth/authSlice";
+
+
+
+export const store = configureStore({
+    reducer:{
+        auth:authReducer,
+
+    },
+})
